899415dfda87eb480fdb06d48aafb4234e0e4f3a
Vladimir Baksheev <baksheev@tochka.com>
2025-10-29 18:52:38 +0500

    Добавил handler для метода update

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,8 +1,12 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+	"github.com/VladimirB/gometrics/internal/handler"
+)
 
 func main() {
+	http.HandleFunc("/update/", handler.MetricsHandler())
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
 		panic(err)
---
+++ internal/handler/metrics.go
@@ -0,0 +1,10 @@
+package handler
+
+import (
+	"net/http"
+)
+
+func MetricsHandler() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+	}
+}
