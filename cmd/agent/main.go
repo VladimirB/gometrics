09cd88071e3836426213707406830c3651b8590e
Vladimir Baksheev <baksheev@tochka.com>
2025-10-30 22:57:58 +0500

    Добавил чтение метрик из runtime каждые две секунды

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -1,10 +1,28 @@
 package main
 
-import "net/http"
+import (
+	"time"
+
+	"github.com/VladimirB/gometrics/internal/agent"
+	model "github.com/VladimirB/gometrics/internal/model"
+)
+
+const (
+	pollInterval = 2
+)
 
 func main() {
-	err := http.ListenAndServe(":8081", nil)
-	if err != nil {
-		panic(err)
+	metrics := make(map[string]model.Metrics)
+
+	pollCounter := 0
+
+	for {
+		pollCounter++
+
+		if pollCounter % pollInterval == 0 {
+			agent.ReadMetrics(metrics)
+		}
+
+		time.Sleep(1 * time.Second)
 	}
 }
