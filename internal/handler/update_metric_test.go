cd60ef95f0df4764c8e55fb0c7562077195790f4
Vladimir Baksheev <baksheev@tochka.com>
2025-11-03 13:15:33 +0500

    Тесты для UpdateMetricHandler

---
+++ internal/handler/update_metric_test.go
@@ -0,0 +1,84 @@
+package handler_test
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/VladimirB/gometrics/internal/handler"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestUpdateMetricHandler(t *testing.T) {
+	type want struct {
+		statusCode int
+	}
+
+	tests := []struct {
+		name string
+		request string
+		want want
+	}{
+		{
+			name: "incorrect path",
+			request: "/update/name/10.45",
+			want: want{
+				statusCode: http.StatusNotFound,
+			},
+		},
+		{
+			name: "success counter metric",
+			request: "/update/counter/name/100",
+			want: want{
+				statusCode: http.StatusOK,
+			},
+		},
+		{
+			name: "success gaige metric",
+			request: "/update/gauge/name/100.33",
+			want: want{
+				statusCode: http.StatusOK,
+			},
+		},
+		{
+			name: "bad request when counter is not int",
+			request: "/update/counter/name/100.555",
+			want: want{
+				statusCode: http.StatusBadRequest,
+			},
+		},
+		{
+			name: "bad request on incorrect gauge metric value",
+			request: "/update/gauge/name/str",
+			want: want{
+				statusCode: http.StatusBadRequest,
+			},
+		},
+		{
+			name: "bad request on incorrect counter metric value",
+			request: "/update/counter/name/str",
+			want: want{
+				statusCode: http.StatusBadRequest,
+			},
+		},
+		{
+			name: "bad request on unknown metric type",
+			request: "/update/unknown/name/100.33",
+			want: want{
+				statusCode: http.StatusBadRequest,
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			request := httptest.NewRequest(http.MethodPost, tt.request, nil)
+			recorder := httptest.NewRecorder()
+			handler := handler.UpdateMetricHandler()
+			handler(recorder, request)
+			result := recorder.Result()
+
+			assert.Equal(t, tt.want.statusCode, result.StatusCode)
+		})
+	}
+}
