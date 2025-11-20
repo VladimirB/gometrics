66090a1c54179b228f7ef703943a1f664d91b54e
Vladimir Baksheev <baksheev@tochka.com>
2025-11-20 23:05:22 +0500

    Добавил логирование ответов сервера

--- internal/handler/request_logger.go
+++ internal/handler/request_logger.go
@@ -7,20 +7,52 @@ import (
 	"github.com/VladimirB/gometrics/internal/logger"
 )
 
+type responseData struct {
+	status int
+	size   int
+}
+
+type loggingResponseWriter struct {
+	http.ResponseWriter
+	responseData *responseData
+}
+
+func (r *loggingResponseWriter) Write(b []byte) (int, error) {
+	size, err := r.ResponseWriter.Write(b)
+	r.responseData.size += size
+	return size, err
+}
+
+func (r *loggingResponseWriter) WriteHeader(statusCode int) {
+	r.ResponseWriter.WriteHeader(statusCode)
+	r.responseData.status = statusCode
+}
+
 func RequestLogger(h http.HandlerFunc) http.HandlerFunc {
 	logFunc := func(w http.ResponseWriter, r *http.Request) {
 		start := time.Now()
 		uri := r.RequestURI
 		method := r.Method
 
-		h.ServeHTTP(w, r)
+		responseData := &responseData{
+			status: 0,
+			size:   0,
+		}
+		loggingResponseWriter := loggingResponseWriter{
+			ResponseWriter: w,
+			responseData:   responseData,
+		}
+
+		h.ServeHTTP(&loggingResponseWriter, r)
 
 		duration := time.Since(start)
 
 		logger.Log.Sugar().Infoln(
 			"uri", uri,
 			"method", method,
+			"status", responseData.status,
 			"duration", duration,
+			"size", responseData.size,
 		)
 	}
 	return http.HandlerFunc(logFunc)
