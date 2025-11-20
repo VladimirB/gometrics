0b793d3604f3dd439d276f1126cad03dca13de42
Vladimir Baksheev <baksheev@tochka.com>
2025-11-20 22:33:03 +0500

    Добавил логгер и его инициализацию

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -8,13 +8,22 @@ import (
 
 	"github.com/VladimirB/gometrics/internal/config"
 	"github.com/VladimirB/gometrics/internal/handler"
+	"github.com/VladimirB/gometrics/internal/logger"
 	"github.com/VladimirB/gometrics/internal/repository"
+	"go.uber.org/zap"
 )
+
+const logLevel = "info"
 
 //go:embed web/template
 var templateFiles embed.FS
 
 func main() {
+	if err := logger.Initialize(logLevel); err != nil {
+		log.Fatal(err)
+	}
+	defer logger.Log.Sync()
+
 	mainPageTemplate := template.Must(template.ParseFS(templateFiles, "web/template/index.html"))
 
 	memStorage := repository.NewMemStorage()
@@ -23,7 +32,7 @@ func main() {
 	valueHandler := handler.NewValueMetricHandler(memStorage)
 
 	config := config.GetServerConfig()
-	log.Println("run server:", config.Address)
+	logger.Log.Info("run server", zap.String("address", config.Address))
 
 	err := http.ListenAndServe(config.Address, handler.NewRouter(mainPageHandler, updateHandler, valueHandler))
 	if err != nil {
