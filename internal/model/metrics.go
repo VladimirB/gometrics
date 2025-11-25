20daf82ac688612cf4ac8bbfde28e79fd46be052
Vladimir Baksheev <baksheev@tochka.com>
2025-11-25 10:14:29 +0500

    Добавил комменты к полям Мetrics

--- internal/model/metrics.go
+++ internal/model/metrics.go
@@ -45,7 +45,7 @@ const (
 type Metrics struct {
 	ID    string   `json:"id"`
 	MType string   `json:"type"`
-	Delta *int64   `json:"delta,omitempty"`
-	Value *float64 `json:"value,omitempty"`
+	Delta *int64   `json:"delta,omitempty"` // значение метрики в случае передачи counter
+	Value *float64 `json:"value,omitempty"` // значение метрики в случае передачи gauge
 	Hash  string   `json:"hash,omitempty"`
 }
