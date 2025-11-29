ba7f512104a27631039f5138317f1d4c71c2d983
Vladimir Baksheev <baksheev@tochka.com>
2025-11-29 19:47:13 +0500

    Отключил компрессию агента

--- internal/metricapi/client.go
+++ internal/metricapi/client.go
@@ -51,16 +51,16 @@ func (c Client) SendAsJSON(server string, metric models.Metrics) error {
 		return err
 	}
 
-	compressedBody, err := compress.Zip(body)
-	if err != nil {
-		return err
-	}
+	// compressedBody, err := compress.Zip(body)
+	// if err != nil {
+	// 	return err
+	// }
 
 	response, err := c.client.R().
 		SetHeader("Content-Type", "application/json").
 		SetHeader("Content-Encoding", "gzip").
 		SetHeader("Accept-Encoding", "gzip").
-		SetBody(compressedBody).
+		SetBody(body).
 		Post(fmt.Sprintf("http://%s/update", server))
 	if err != nil {
 		return err
