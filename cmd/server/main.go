1d33b67b6122fc89831fcf77a61d58b7792f9218
Vladimir Baksheev <baksheev@tochka.com>
2025-12-06 17:22:06 +0500

    Добавил таймер на запись метрик в файл

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -27,7 +27,7 @@ func main() {
 	defer logger.Log.Sync()
 
 	config := config.GetServerConfig()
-	logger.Log.Info("Running Server", zap.String("Start time", time.Now().Local().String()), zap.String("address", config.Address))
+	logger.Log.Info("Running Server", zap.String("Start time", time.Now().Local().String()), zap.Any("config", config))
 
 	mainPageTemplate := template.Must(template.ParseFS(templateFiles, "web/template/index.html"))
 
@@ -38,8 +38,22 @@ func main() {
 	updateHandler := handler.NewUpdateMetricHandler(metricsService)
 	valueHandler := handler.NewValueMetricHandler(metricsService)
 
+	go runMetricsWriter(config.StoreInterval)
+
 	err := http.ListenAndServe(config.Address, handler.NewRouter(mainPageHandler, updateHandler, valueHandler))
 	if err != nil {
 		logger.Log.Fatal(err.Error())
+	}
+}
+
+func runMetricsWriter(interval time.Duration) {
+	storeInFileTicker := time.NewTicker(interval)
+	defer storeInFileTicker.Stop()
+
+	for {
+		select {
+		case <-storeInFileTicker.C:
+			logger.Log.Info("Write metrics to file")
+		}
 	}
 }
