6ee9ad2b44b59bd319a4712987bf26be0190b22d
Vladimir Baksheev <baksheev@tochka.com>
2025-12-07 18:39:30 +0500

    Правки статиков и конфига

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -55,11 +55,13 @@ func initMetricsFromFile(fileName string, metricsService *service.MetricsService
 	jsonFileReader, err := repository.NewMetricsFileReader(fileName)
 	if err != nil {
 		logger.Log.Error("Cant open file to read metrics", zap.Error(err))
+		return
 	}
 
 	result, err := jsonFileReader.Read()
 	if err != nil {
 		logger.Log.Error("Cant read metrics from file", zap.Error(err))
+		return
 	}
 
 	for _, m := range result {
@@ -74,31 +76,29 @@ func runStoreInFileTicker(config config.ServerConfig, service *service.MetricsSe
 	storeInFileTicker := time.NewTicker(config.StoreInterval)
 	defer storeInFileTicker.Stop()
 
-	for {
-		select {
-		case <-storeInFileTicker.C:
-			logger.Log.Info("Write metrics to file")
+	for tick := range storeInFileTicker.C {
+        logger.Log.Info("Write metrics to file", zap.Any("Seconds from start", tick.Second()))
 
-			currentMetrics := service.GetAll()
-			if len(currentMetrics) == 0 {
-				return
-			}
-
-			fileWriter, err := repository.NewMetricsFileWriter(config.FileStoragePath)
-			if err != nil {
-				logger.Log.Fatal("Cant initialize metrics file writer", zap.Error(err))
-			}
-			defer fileWriter.Close()
-
-			var metrics = make([]models.Metrics, 0)
-			for _, value := range currentMetrics {
-				metrics = append(metrics, value)
-			}
-
-			err = fileWriter.Write(metrics)
-			if err != nil {
-				logger.Log.Error("Error on metrics write to file", zap.Error(err))
-			}
+		currentMetrics := service.GetAll()
+		if len(currentMetrics) == 0 {
+			continue
 		}
-	}
+
+		fileWriter, err := repository.NewMetricsFileWriter(config.FileStoragePath)
+		if err != nil {
+			logger.Log.Fatal("Cant initialize metrics file writer", zap.Error(err))
+			continue
+		}
+
+		var metrics []models.Metrics
+		for _, value := range currentMetrics {
+			metrics = append(metrics, value)
+		}
+
+		err = fileWriter.Write(metrics)
+		if err != nil {
+			logger.Log.Error("Error on metrics write to file", zap.Error(err))
+		}
+		fileWriter.Close()
+    }
 }
--- internal/config/server_config.go
+++ internal/config/server_config.go
@@ -11,7 +11,7 @@ import (
 
 const (
 	defaultServerAddress   = "localhost:8080"
-	defaultStoreInterval   = 5
+	defaultStoreInterval   = 300
 	defaultFileStoragePath = "metrics.json"
 	defaultRestore         = true
 )
