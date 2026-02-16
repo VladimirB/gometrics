a2c213e567f8e9f308cedda6c80002cb7ef38b60
Vladimir Baksheev <baksheev@tochka.com>
2026-02-16 15:26:05 +0500

    Увеличил кол-во символов в id метрики в БД

--- migrations/000001_create_metrics_table.up.sql
+++ migrations/000001_create_metrics_table.up.sql
@@ -4,7 +4,7 @@ CREATE TYPE metric_type AS ENUM ('gauge', 'counter');
 
 -- Таблица для хранения метрик
 CREATE TABLE metrics (
-    id VARCHAR(15) PRIMARY KEY,
+    id VARCHAR(30) PRIMARY KEY,
     type metric_type NOT NULL,
     delta BIGINT,
     value DOUBLE PRECISION
